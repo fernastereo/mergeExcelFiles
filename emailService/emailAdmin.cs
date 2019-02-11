62f3f7e6ab4fd60f5b339c1e33b709842512ed43
fernastereo <fernandoecueto@gmail.com>
2019-02-11 13:31:19 -0500

    Implementado constructor de la clase emailAdmin

--- emailService/emailAdmin.cs
+++ emailService/emailAdmin.cs
@@ -1,28 +1,38 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
-
 
 namespace mergeExcelFiles
 {
     public class emailAdmin
     {
         private SmtpClient client;
-        private MailMessage email;
+        private MailMessage mail;
+        private string _host;
+        private int _port;
+        private string _user;
+        private string _pass;
+        private bool _essl;
 
-        public emailAdmin()
+        public emailAdmin(string host, int port, string user, string pass, bool essl)
         {
-            client = new SmtpClient();
-        }
+            _host = host;
+            _port = port;
+            _user = user;
+            _pass = pass;
+            _essl = essl;
 
-        private static void inicializaConfiguracion()
-        {
-            
+            client = new SmtpClient(_host, _port) {
+                EnableSsl = _essl,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_user, _pass)
+            };
         }
     }
 }
