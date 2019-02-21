226d9f8c74f81682ae7b8a98d4dcfacac3f7f18b
fernastereo <fernandoecueto@gmail.com>
2019-02-21 09:08:01 -0500

    Creada interfaz para administrar parametros de correo

--- mergeExcelFiles/frmMerge.Designer.cs
+++ mergeExcelFiles/frmMerge.Designer.cs
@@ -61,11 +61,24 @@
             this.worksheet = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.initrow = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.endrow = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnUpdateMailSettings = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.configDataBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.excelFilesDataSet)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFileDefinition)).BeginInit();
             this.SuspendLayout();
@@ -125,6 +138,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Controls.Add(this.dgvFileDefinition);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
@@ -136,6 +150,17 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.txtSender);
+            this.tabPage3.Controls.Add(this.txtPassword);
+            this.tabPage3.Controls.Add(this.txtUser);
+            this.tabPage3.Controls.Add(this.txtPort);
+            this.tabPage3.Controls.Add(this.txtHost);
+            this.tabPage3.Controls.Add(this.label9);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.btnUpdateMailSettings);
+            this.tabPage3.Controls.Add(this.label5);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -360,6 +385,105 @@
             this.endrow.Name = "endrow";
             this.endrow.ReadOnly = true;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Servidor Correo Saliente:";
+            // 
+            // btnUpdateMailSettings
+            // 
+            this.btnUpdateMailSettings.Location = new System.Drawing.Point(536, 214);
+            this.btnUpdateMailSettings.Name = "btnUpdateMailSettings";
+            this.btnUpdateMailSettings.Size = new System.Drawing.Size(108, 23);
+            this.btnUpdateMailSettings.TabIndex = 10;
+            this.btnUpdateMailSettings.Text = "A&ctualizar";
+            this.btnUpdateMailSettings.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Puerto:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Usuario:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Password:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 147);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Nombre Remitente";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(147, 23);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(224, 20);
+            this.txtHost.TabIndex = 15;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(147, 55);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(45, 20);
+            this.txtPort.TabIndex = 16;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(147, 85);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(224, 20);
+            this.txtUser.TabIndex = 17;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(147, 114);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(224, 20);
+            this.txtPassword.TabIndex = 18;
+            // 
+            // txtSender
+            // 
+            this.txtSender.Location = new System.Drawing.Point(147, 144);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(224, 20);
+            this.txtSender.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "A&ctualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -379,6 +503,8 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFileDefinition)).EndInit();
@@ -418,6 +544,18 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn initrow;
         private System.Windows.Forms.DataGridViewTextBoxColumn endrow;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdateMailSettings;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtHost;
     }
 }
 
